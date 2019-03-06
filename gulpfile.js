89f0cb0426ed48cdfd67718281cf140e59263104
Vitalii Shumeiko <vitaliy.sh.82@gmail.com>
2019-03-06 22:47:11 +0200

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-csso');
-var htmlmin = require('gulp-htmlmin');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var obfuscateagain =  require('gulp-javascript-obfuscator');
-var image = require('gulp-image');
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const less = require('gulp-less');
+const minifyCSS = require('gulp-csso');
+const image = require('gulp-image');
 
-gulp.task('html', function(){
+gulp.task('minify', () => {
     return gulp.src('*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('build/html'))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build/html'));
 });
 
 gulp.task('css', function(){
@@ -20,20 +17,10 @@ gulp.task('css', function(){
         .pipe(gulp.dest('build/css'))
 });
 
-gulp.task('js', function(){
-    return gulp.src('js/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(obfuscateagain())
-        .pipe(concat('app.min.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/js'))
-});
-
 gulp.task('img', function(){
     return gulp.src('img/* ')
         .pipe(image())
         .pipe(gulp.dest('build/img'))
 });
 
-
-gulp.task('default',  [ 'html', 'css', 'js', 'img' ]);
+gulp.task('default', gulp.parallel('minify', 'css', 'img'))
